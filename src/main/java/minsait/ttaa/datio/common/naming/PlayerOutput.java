032f78d1fd534e36ff16796762e2c2682268af3c
mlozanoe <89434015+mlozanoe@users.noreply.github.com>
2021-08-24 20:15:53 -0500

    Add files via upload

--- src/main/java/minsait/ttaa/datio/common/naming/PlayerOutput.java
+++ src/main/java/minsait/ttaa/datio/common/naming/PlayerOutput.java
@@ -2,7 +2,7 @@ package minsait.ttaa.datio.common.naming;
 
 public final class PlayerOutput {
 
-    public static Field playerCat = new Field("player_cat");
+    public static Field playerCat = new Field("playerCat");
     public static Field potential_vs_overall = new Field("potential_vs_overall");
 
 }
